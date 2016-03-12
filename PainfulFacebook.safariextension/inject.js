9970d13cab5d996aa52ea3403ff1d5330e179d17
Manfred Touron <m@42.am>
2016-03-12 21:28:32 +0100

    Restrict extension on timeline

--- PainfulFacebook.safariextension/inject.js
+++ PainfulFacebook.safariextension/inject.js
@@ -1,9 +1,13 @@
 (function() {
+  if (document.location.pathname !== "/") {
+    return;
+  }
+
   var lastPosition = 0;
   var safeZone = 1000;
 
   window.setInterval(function() {
-    var y = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    var y = window.pageYOffset || document.scrollTop || document.body.scrollTop || 0;
     if (y >= lastPosition + safeZone) {
       alert("au boulot !");
       lastPosition = y;
