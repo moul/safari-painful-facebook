f8fe0a825d4f3dd2897e6dc5f1784b1d83b87276
Manfred Touron <m@42.am>
2016-03-12 21:42:09 +0100

    Disable settings

--- PainfulFacebook.safariextension/inject.js
+++ PainfulFacebook.safariextension/inject.js
@@ -4,9 +4,10 @@
   }
 
   var lastPosition = 0;
-  var safeZone = 1000;
+  var safeZone = 3000;
 
   window.setInterval(function() {
+    if (document.body === undefined) { return; }
     var y = window.pageYOffset || document.scrollTop || document.body.scrollTop || 0;
     if (y >= lastPosition + safeZone) {
       alert("au boulot !");
