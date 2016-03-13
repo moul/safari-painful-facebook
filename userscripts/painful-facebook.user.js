8b9ac04495b4abaa2cfb34a2ae88444107b6bbc8
Manfred Touron <m@42.am>
2016-03-13 08:00:48 +0100

    Add userscript

---
+++ userscripts/painful-facebook.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name           Painful Facebook
+// @namespace      http://m.42.am/
+// @description    Facebook timeline + Pain = Productivity !
+// @include        https://www.facebook.com/*
+// ==/UserScript==
+
+
+(function() {
+  if (document.location.pathname !== "/") {
+    return;
+  }
+
+  var lastPosition = 0;
+  var safeZone = 3000;
+
+  window.setInterval(function() {
+    if (document.body == null) { return; }
+    var y = window.pageYOffset || document.scrollTop || document.body.scrollTop || 0;
+    if (y >= lastPosition + safeZone) {
+      alert("au boulot !");
+      lastPosition = y;
+    }
+  }, 1000);
+})();
